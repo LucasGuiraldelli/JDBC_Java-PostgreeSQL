d4b701319dafb7b55e9be08e32e1012061d5d0e2
Lucas Guiraldelli <64923139+LucasGuiraldelli@users.noreply.github.com>
2021-09-11 23:21:57 -0300

    Add files via upload

---
+++ JDBCPostgree/src/Application/Principal.java
@@ -0,0 +1,12 @@
+package Application;
+
+import java.sql.Connection;
+import Connection.connection;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		connection conn = new connection();
+	}
+
+}
